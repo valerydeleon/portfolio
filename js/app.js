74f47f631ad82a116b3adf2c5e471bc069c00738
Valeria de Leon <valery.deleon@gmail.com>
2017-07-25 23:47:39 -0500

    modals

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-var loadPage = function(){
-  navOnChange();
-};
-
-var navOnChange = function(){
-  var navBar = $("#nav-bar");
-  if (window.scrollY > 500){
-    navBar.removeClass("uk-navbar-transparent");
-  } else if (window.scrollY < 100){
-    navBar.addClass("uk-navbar-transparent")
-  }
-};
-
-document.addEventListener("scroll", navOnChange);
-
-$(document).ready(loadPage);
+// var loadPage = function(){
+//   navOnChange();
+// };
+//
+// var navOnChange = function(){
+//   var navBar = $("#nav-bar");
+//   if (window.scrollY > 861){
+//     navBar.removeClass("uk-navbar-transparent");
+//       navBar.removeClass(".uk-navbar-nav>li.uk-active.text-white>a");
+//       $(".uk-navbar-nav>li.uk-active.text-white a").addClass(".uk-navbar-nav>li.uk-active.text-blue a");
+//   } else if (window.scrollY < 100){
+//     navBar.addClass("uk-navbar-transparent")
+//   }
+// };
+//
+// document.addEventListener("scroll", navOnChange);
+//
+// $(document).ready(loadPage);
