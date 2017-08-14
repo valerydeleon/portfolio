84552cf62520ae8179d76e4d8689ced380f85ffb
Valeria de Leon <valery.deleon@gmail.com>
2017-08-14 16:44:09 -0500

    mobile navbar

--- js/app.js
+++ js/app.js
@@ -3,17 +3,24 @@ var navLinkServices = document.getElementById("nav-link-services");
 var navLinkProjects = document.getElementById("nav-link-projects");
 var navButton = document.getElementById("nav-button");
 
+var navMobileIcon = document.getElementById("nav-mobile-icon");
+var navMobileText = document.getElementById("nav-mobile-text");
+
 var navColorChange = function(){
   if (window.scrollY > 550) {
     navContainer.style.backgroundColor = "#f5f5f5";
     navLinkServices.style.color = "#2D3483";
     navLinkProjects.style.color = "#2D3483";
     navButton.style.color = "#2D3483";
+    navMobileIcon.style.color = "#2D3483";
+    navMobileText.style.color = "#2D3483";
   } else {
     navContainer.style.backgroundColor = "rgba(255, 255, 255, 0)";
     navLinkServices.style.color = "#e3e3e3";
     navLinkProjects.style.color = "#e3e3e3";
     navButton.style.color = "#e3e3e3";
+    navMobileIcon.style.color = "#e3e3e3";
+    navMobileText.style.color = "#e3e3e3";
   }
 };
 document.addEventListener("scroll", navColorChange);
