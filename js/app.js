af7c058ed7ca920c9a60115aef31ea2862067b9b
Valeria de Leon <valery.deleon@gmail.com>
2017-08-14 14:04:14 -0500

    funcionalidad navbar y modal

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-// var loadPage = function(){
-//   navOnChange();
-// };
-//
-// var navOnChange = function(){
-//   var navBar = $("#nav-bar");
-//   if (window.scrollY > 861){
-//     navBar.removeClass("uk-navbar-transparent");
-//       navBar.removeClass(".uk-navbar-nav>li.uk-active.text-white>a");
-//       $(".uk-navbar-nav>li.uk-active.text-white a").addClass(".uk-navbar-nav>li.uk-active.text-blue a");
-//   } else if (window.scrollY < 100){
-//     navBar.addClass("uk-navbar-transparent")
-//   }
-// };
-//
-// document.addEventListener("scroll", navOnChange);
-//
-// $(document).ready(loadPage);
+var navContainer = document.getElementById("nav-container");
+var navLinkServices = document.getElementById("nav-link-services");
+var navLinkProjects = document.getElementById("nav-link-projects");
+var navButton = document.getElementById("nav-button");
+
+var navColorChange = function(){
+  if (window.scrollY > 550) {
+    navContainer.style.backgroundColor = "#f5f5f5";
+    navLinkServices.style.color = "#2D3483";
+    navLinkProjects.style.color = "#2D3483";
+    navButton.style.color = "#2D3483";
+  } else {
+    navContainer.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    navLinkServices.style.color = "#e3e3e3";
+    navLinkProjects.style.color = "#e3e3e3";
+    navButton.style.color = "#e3e3e3";
+  }
+};
+document.addEventListener("scroll", navColorChange);
